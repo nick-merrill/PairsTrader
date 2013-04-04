795f65c9878977f0ebb6521d6e7c6217463b1342
Nick Merrill <nicholas.e.merrill@gmail.com>
2013-04-04 11:39:49 -0400

    Some stuff but found better resource so switching

--- stockGrabber.py
+++ stockGrabber.py
@@ -1,3 +1,7 @@
+import urllib2
+urlopen = urllib2.request.urlopen
+urlEncode = urllib2.urlencode
+
 # From example at http://developer.yahoo.com/yql/guide/yql-code-examples.html#sdk_yql
 
 BASE_URL = "http://query.yahooapis.com/v1/yql"
@@ -12,4 +16,11 @@ def handler(rsp):
     if rsp.data:
         results = rsp.data
 
+def toQueryString(obj):
+    parts = []
+    for each in obj:
+        if obj.hasOwnProperty(each):
+            parts.push(urlEncode(each) + '=' + urlEncode(obj[each]))
+    return parts.join('&')
 
+urlopen(BASE_URL)
