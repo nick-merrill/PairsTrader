eb36ecc588348d370cb9e71967eac95d99c28dc7
Nick Merrill <nicholas.e.merrill@gmail.com>
2013-04-04 17:26:17 -0400

    More attempts.

--- stockGrabber.py
+++ stockGrabber.py
@@ -1,26 +1,7 @@
-import urllib2
-urlopen = urllib2.request.urlopen
-urlEncode = urllib2.urlencode
+#!/usr/bin/python
 
-# From example at http://developer.yahoo.com/yql/guide/yql-code-examples.html#sdk_yql
+from yql import *
 
-BASE_URL = "http://query.yahooapis.com/v1/yql"
+q = Lookup(["AAPL"])
 
-results = ""
-
-# query to get airport data for SFO airport
-query = "SELECT * from geo.places WHERE text='SFO'"
-
-# callback function for handling response data
-def handler(rsp):
-    if rsp.data:
-        results = rsp.data
-
-def toQueryString(obj):
-    parts = []
-    for each in obj:
-        if obj.hasOwnProperty(each):
-            parts.push(urlEncode(each) + '=' + urlEncode(obj[each]))
-    return parts.join('&')
-
-urlopen(BASE_URL)
+q.submit()
--- yql.py
+++ yql.py
@@ -2,30 +2,54 @@ import urllib
 import urllib2
 
 YQL_BASE = "http://query.yahooapis.com/v1/public/yql?"
+QUOTES_DB = "yahoo.finance.quotes"
 
 class Query:
     output = "json"
 
-    def build_url(self):
+    def yql_format(self):
         raise "Abstract"
 
     def submit(self):
-        url = self.build_url()
-        response = urllib2.Request(url).read()
+        yql_query = self.yql_format()
+        print yql_query
+        url = YQL_BASE + urllib.urlencode([("q", yql_query)])
+        print url
+        response = urllib2.urlopen(url).read()
         return response
 
+class Date:
+    year = 0
+    month = 0
+    day = 0
+    
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
 
-class SingleQuery(Query):
-    stock = ""
+    def yql_format(self):
+        # TODO: this has improper formatting (four digit year, etc.)
+        # use python date class
+        return str(self.year) + '-' + str(self.month) + '-' + str(self.day)
 
-    def __init__(self, stock):
-        self.stock = stock
-
-    def build_url(self):
-        return YQL_BASE + urllib.urlencode([("stock", stock)])
-
-class MultipleQuery(Query):
+class Lookup(Query):
     stocks = []
+
+    start_date = None
+    end_date = None
 
     def __init__(self, stocks):
         self.stocks = stocks
+
+    def yql_format(self):
+        stocks = []
+        for stock in self.stocks:
+            stocks.append('"' + stock + '"')
+        yql_query = "select * from " + QUOTES_DB + " where symbol in (" \
+                     + (",".join(stocks)) + ")"
+        if start_date:
+            yql_query += ' and startDate = "' + self.start_date.yql_format() + '"'
+        if end_date:
+            yql_query += ' and startDate = "' + self.end_date.yql_format() + '"'
+        return yql_query
