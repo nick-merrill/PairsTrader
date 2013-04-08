8c46214855a6fac559477c15a7b9db26e01bc1f5
Nick Merrill <nicholas.e.merrill@gmail.com>
2013-04-08 17:48:35 -0400

    Added combination generator.

---
+++ combinationGenerator.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import sys
+from os import path
+import csv
+import itertools
+
+def csv_to_single_array(csv_file, col_num):
+    rows = []
+    reader = csv.reader(csv_file)
+    for row in reader:
+        print row
+        print "\n\n\n"
+        try:
+            rows.append(row[col_num])
+        except IndexError:
+            pass
+    return rows
+
+try:
+    symbols_file_name = sys.argv[1]
+    out_name = sys.argv[2]
+except IndexError:
+    if len(sys.argv) < 3:
+        print "You must pass the symbols' file name AND an outfile name."
+        sys.exit()
+
+with open(symbols_file_name, "rb") as f:
+    symbols = csv_to_single_array(f, 0)
+    symbols.pop(0)
+    f.close()
+
+combos_iterable = itertools.combinations(symbols, 2)
+
+combos = []
+for c in combos_iterable:
+    combos.append(c)
+
+with open(out_name, "wb") as out:
+    writer = csv.writer(out)
+    writer.writerows(combos)
+    out.close()
+
+
+
+
